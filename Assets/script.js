30c05cdb3487200e7b6e614a0d586647e03a8b3d
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-09-19 12:05:38 -0700

    fixed the size of the images for the save button, added style in html because honestly its way easier and simpler than putting it in CSS. Also added classes, id and tags to objects

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,8 @@
 var today = moment();
 $("#todays-date").text(today.format("dddd, MMM Do, YYYY  h:mm:ss A"));
+
+//place all the tags here:
+
+
+
+//make a function to save whatever is in the textbox
